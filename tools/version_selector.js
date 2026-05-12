a60abde05e0de108b82990ed501576795d02c086
ローリエ1111 <5r4zpkczvj@privaterelay.appleid.com>
2026-05-13 05:28:50 +0900

    Update version_selector.js

--- tools/version_selector.js
+++ tools/version_selector.js
@@ -2,39 +2,39 @@
 (function(global) {
     const VERSIONS = [
         {
-            version: 'v1.5.5',
+            version: 'ver1.5.5',
             date: '2026-05-12',
-            url: 'https://gist.githubusercontent.com/yuffy-1111/38c5738de4204952cc9b3365a93240d4/raw/04a708450969daf6927e03bd0a8ec5c1b9de8fa3/ver155_hb_edition.html',
-            desc: 'はてなブログ版'
+            url: 'https://htmlpreview.github.io/?https://gist.githubusercontent.com/yuffy-1111/38c5738de4204952cc9b3365a93240d4/raw/04a708450969daf6927e03bd0a8ec5c1b9de8fa3/ver155_hb_edition.html',
+            desc: 'はてなブログ版1.5.5'
         },
         {
-            version: 'v1.4.5',
+            version: 'ver1.4.5',
             date: '2026-04-xx',
-            url: 'https://gist.githubusercontent.com/yuffy-1111/5eddb25d14fb2af57a8ad2eedc979de3/raw/16aa21ec6b7d0829ead7e70a292830a8e2ca6839/ver145.html',
+            url: 'https://htmlpreview.github.io/?https://gist.githubusercontent.com/yuffy-1111/5eddb25d14fb2af57a8ad2eedc979de3/raw/16aa21ec6b7d0829ead7e70a292830a8e2ca6839/ver145.html',
             desc: ''
         },
         {
-            version: 'v1.4.1',
+            version: 'ver1.4.1',
             date: '2026-04-xx',
-            url: 'https://gist.githubusercontent.com/yuffy-1111/bc042c561726813e1fd18f8cf0515ae6/raw/894fa7a3955ec17eee6d1b13e725a2c0f09f998c/ver141web(delete).html',
-            desc: 'ウェブフック版'
-        },
-        {
-            version: 'v1.3.0',
-            date: '2026-03-xx',
-            url: 'https://gist.githubusercontent.com/yuffy-1111/ae466bdca9582b41332bd2e7e4d29a65/raw/a3acbd51a0a0e7dce5468d5fd4e60dcbfe2aa4f8/ver130.html',
+            url: 'https://htmlpreview.github.io/?https://gist.githubusercontent.com/yuffy-1111/bc042c561726813e1fd18f8cf0515ae6/raw/894fa7a3955ec17eee6d1b13e725a2c0f09f998c/ver141web(delete).html',
             desc: ''
         },
         {
-            version: 'v1.2.6',
+            version: 'ver1.3.0',
             date: '2026-03-xx',
-            url: 'https://gist.githubusercontent.com/yuffy-1111/9fe76e96218225e6486481067c9b9664/raw/a6fc8191604a3a7d55ec253c1681e4b0510ee04a/ver126b.html',
+            url: 'https://htmlpreview.github.io/?https://gist.githubusercontent.com/yuffy-1111/ae466bdca9582b41332bd2e7e4d29a65/raw/a3acbd51a0a0e7dce5468d5fd4e60dcbfe2aa4f8/ver130.html',
+            desc: ''
+        },
+        {
+            version: 'ver1.2.6',
+            date: '2026-03-xx',
+            url: 'https://htmlpreview.github.io/?https://gist.githubusercontent.com/yuffy-1111/9fe76e96218225e6486481067c9b9664/raw/a6fc8191604a3a7d55ec253c1681e4b0510ee04a/ver126b.html',
             desc: 'Blue Edition'
         },
         {
-            version: 'v1.1.7',
+            version: 'ver1.1.7',
             date: '2026-02-xx',
-            url: 'https://gist.githubusercontent.com/yuffy-1111/332d9aab87f418311f8a961ad35b6bec/raw/bccc31c9d9ef17f6633db2927b752c2784b573c2/ver117.html',
+            url: 'https://htmlpreview.github.io/?https://gist.githubusercontent.com/yuffy-1111/332d9aab87f418311f8a961ad35b6bec/raw/bccc31c9d9ef17f6633db2927b752c2784b573c2/ver117.html',
             desc: ''
         }
     ];
