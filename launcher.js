51594c9e64410361a95c3ef3b09ac4752e1c5d3c
ローリエ1111 <5r4zpkczvj@privaterelay.appleid.com>
2026-05-15 06:07:56 +0900

    Update launcher.js

--- launcher.js
+++ launcher.js
@@ -1,6 +1,6 @@
 // ==========ツールランチャー（改造版）=========
 // ========== バージョン管理 ==========
-const APP_VERSION = '20260516';
+const APP_VERSION = '20260517';
 
 function checkVersionUpdate() {
     const storedVersion = localStorage.getItem('dqx_app_version');
