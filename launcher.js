62892c6000bbe49b1a5f79d1853514c51678ce9e
ローリエ1111 <5r4zpkczvj@privaterelay.appleid.com>
2026-07-25 01:17:52 +0900

    Update launcher.js

--- launcher.js
+++ launcher.js
@@ -1,5 +1,5 @@
 // ========== DQXTools ランチャー ==========
-const APP_VERSION = '1.1.5s';
+const APP_VERSION = '1.0.0β';
 window.LAUNCHER_VERSION = APP_VERSION;
 
 // ランチャー読み込み完了を通知（index.html 側が受信してバージョン確認を行う）
